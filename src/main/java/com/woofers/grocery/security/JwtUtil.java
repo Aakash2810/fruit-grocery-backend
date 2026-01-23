899cb533899f7570a5c0de2327dfdfc2f238e5c3
unknown <aakashverma2810@gmail.com>
2026-01-24 00:52:21 +0530

    added secret key

--- src/main/java/com/woofers/grocery/security/JwtUtil.java
+++ src/main/java/com/woofers/grocery/security/JwtUtil.java
@@ -1,9 +1,11 @@
 package com.woofers.grocery.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.woofers.grocery.entity.User;
@@ -11,30 +13,41 @@ import com.woofers.grocery.entity.User;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.crypto.SecretKey;
+import java.security.Key;
 
 @Component
 public class JwtUtil {
 
-	 private static final SecretKey KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secret;
 
-	 public String generateToken(User user) {
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
 
-		    Map<String, Object> claims = new HashMap<>();
-		    claims.put("role", user.getRole().name());
+    public String generateToken(User user) {
 
-		    return Jwts.builder()
-		            .setClaims(claims)
-		            .setSubject(user.getUsername())
-		            .setIssuedAt(new Date())
-		            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-		            .signWith(KEY, SignatureAlgorithm.HS256)
-		            .compact();
-		}
-	    public String extractUsername(String token) {
-	        return Jwts.parserBuilder().setSigningKey(KEY).build()
-	                .parseClaimsJws(token)
-	                .getBody().getSubject();
-	    }
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", user.getRole().name());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
